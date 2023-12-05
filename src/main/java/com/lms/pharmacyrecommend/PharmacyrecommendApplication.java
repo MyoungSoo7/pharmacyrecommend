24d8c496bb60aaeae15030578d8eb7e45d6a67ec
fastcamedu <iamipro@naver.com>
2023-12-05 10:25:41 +0900

    init

---
+++ src/main/java/com/lms/pharmacyrecommend/PharmacyrecommendApplication.java
@@ -0,0 +1,13 @@
+package com.lms.pharmacyrecommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyrecommendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacyrecommendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lms/pharmacyrecommend/PharmacyrecommendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms.pharmacyrecommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyrecommendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
